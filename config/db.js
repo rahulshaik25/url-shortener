38392036c26afc47cd5ab6f8e2b69c7fefd9a18d
rahul1-cv <rahulshaik25@gmail.com>
2026-07-16 20:34:54 +0530

    Configured this to Aiven cloud database

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 const mariadb = require("mariadb");
+const fs = require("fs");
+const path = require("path");
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, "ca.pem"), "utf8")
+    },
 
     connectionLimit: 5
 });
