4dcf8a9e9c002d7fa075899161f461a2b186e165
Rahul Shaik <142885560+rahulshaik25@users.noreply.github.com>
2026-07-16 20:58:14 +0530

    both local and hosting works db

--- config/db.js
+++ config/db.js
@@ -2,18 +2,21 @@ const mariadb = require("mariadb");
 const fs = require("fs");
 const path = require("path");
 
-const pool = mariadb.createPool({
+const config = {
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-
-    ssl: {
-        ca: fs.readFileSync(path.join(__dirname, "ca.pem"), "utf8")
-    },
-
     connectionLimit: 5
-});
+};
+
+if (process.env.DB_HOST !== "localhost") {
+    config.ssl = {
+        ca: fs.readFileSync(path.join(__dirname, "ca.pem"), "utf8")
+    };
+}
+
+const pool = mariadb.createPool(config);
 
 module.exports = pool;
